4d1a59b4b6faed63be572e8a92a65d63e271ce72
oscar <meet@oscarfdez.com>
2022-04-03 20:16:31 +0200

    create a scss layout system fix col <900

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import "./styles/main.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola</h1>
+    <div id='layout'>
+    <header>
+      header
+    </header>
+    <main className='container grid'>
+      <div className='col-12 col-md-4 col-sm-6 col-xs-12'>main</div>
+    </main>
+    <footer>
+      footer
+    </footer>
     </div>
   );
 }
