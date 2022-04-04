ef5e9d79ae2907df274ddd85cfdd565873620120
oscar <meet@oscarfdez.com>
2022-04-04 23:31:03 +0200

    fetch api planets

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Avatar from './Avatar'
-import Img from "../assets/planets/bespin.png"
+import Img from "../assets/people/luke-skywalker.png"
 
 const Card = ({src, alt, title, subtitle, caption}) => {
+
   return (
     <div className='card'>
         <div className='card__image'>
@@ -12,7 +13,7 @@ const Card = ({src, alt, title, subtitle, caption}) => {
             <div className='card__info-general'>
                 <h3>{title}</h3>
                 <span>{subtitle}</span>
-                <caption className='mt-4'>{caption}</caption>
+                <caption className='mt-4'>Population: {caption}</caption>
             </div>
             <div className='card__info-avatar'>
                 <Avatar src={Img} alt='planet-image'/>
